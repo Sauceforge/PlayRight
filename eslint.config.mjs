a6d4aa928d7bef34b233c732572de0d70b3eed86
Andrew Gray <andrew.paul.gray@gmail.com>
2025-09-26 17:14:17 +1000

    upgrade to eslint-config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["**/node_modules/"]), {
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:playwright/recommended",
+        "plugin:prettier/recommended",
+    ),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: ["./tsconfig.json"],
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/no-var-requires": "off",
+        "arrow-body-style": "off",
+        "prefer-arrow-callback": "off",
+        "playwright/prefer-web-first-assertions": "off",
+    },
+}]);
